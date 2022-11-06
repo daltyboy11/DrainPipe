5ffc0033f85cd2cf4ce640ef14d1aabf228c509b
Dalton Sweeney <daltyboy11@gmail.com>
2022-11-05 20:55:48 -0700

    allow cors in server

--- server.py
+++ server.py
@@ -4,10 +4,19 @@ from subprocess import Popen
 import pdb
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RequesterData(BaseModel):
     contract_address: str
